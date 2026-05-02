1470b0cc0987504bf2b74ba481282c07d0976ad0
Mariana Gomes <marianna.gomez2013@gmail.com>
2026-05-02 12:26:29 -0300

    Create main.js

---
+++ Semana01/main.js
@@ -0,0 +1,26 @@
+let a = 5;
+let b = 10;
+
+let resultado = a + b
+
+// Comentário não executável
+/*
+Comentário não executável
+*/
+
+//Função de Inicialização. Não retorna nada.
+function iniciarBancoDados() {
+
+}
+
+//Função que passamos parâmetros, ela recebe algo
+function soma(a, b) {
+    console.log(a+b)
+}
+
+
+//Função com retorno de algo, passamos parâmetro nela e vemos que a tipagem virou number
+function subtracao(a, b) {
+    return a - b
+}
+
